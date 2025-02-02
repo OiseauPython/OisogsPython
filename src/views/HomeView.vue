ac014960ddf727880d65a6ff2c5e7a1d46003d40
OiseauPython <jeremy.stanko@posteo.net>
2025-02-02 21:48:41 +0100

    Init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>Fichier Discogs au format CSV :</p>
+    <input type="file" name="CSV" id="">
+    <input type="button" value="Soumettre">
+  </main>
+</template>
